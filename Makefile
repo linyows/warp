f6f6ff874e05479e88c9c30d926430e597c274de
linyows <linyows@gmail.com>
2021-01-20 09:33:22 +0900

    use goreleaser

--- Makefile
+++ Makefile
@@ -1,7 +1,18 @@
+SOURCE_FILES?=./...
+TEST_PATTERN?=.
+TEST_OPTIONS?=
+
 default: build
+
+setup:
+	go mod download
 
 build:
 	env GOOS=linux GOARCH=amd64 go build -o warp ./cmd/warp/main.go
 
 test:
-	go test -v -cover
+	go test $(TEST_OPTIONS) -failfast -race -coverpkg=./... -covermode=atomic \
+		-coverprofile=coverage.txt $(SOURCE_FILES) -run $(TEST_PATTERN) -timeout=5m
+
+release:
+	@test -z $(GITHUB_TOKEN) || goreleaser --snapshot --rm-dist
--- cmd/warp/main.go
+++ cmd/warp/main.go
@@ -2,19 +2,48 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 
 	"github.com/linyows/warp"
 )
 
-func main() {
-	var (
-		ip   = flag.String("ip", "127.0.0.1", "listen ip")
-		port = flag.Int("port", 0, "listen port")
-	)
+var (
+	version = "dev"
+	commit  = ""
+	date    = ""
+	builtBy = ""
+	ip      = flag.String("ip", "127.0.0.1", "listen ip")
+	port    = flag.Int("port", 0, "listen port")
+	verFlag = flag.Bool("version", false, "show build version")
+)
+
+func init() {
 	flag.Parse()
+}
+
+func main() {
+	if *verFlag {
+		fmt.Fprintf(os.Stderr, buildVersion(version, commit, date, builtBy)+"\n")
+		return
+	}
 	w := &warp.Server{Addr: *ip, Port: *port}
 	err := w.Start()
 	if err != nil {
 		panic(err)
 	}
+}
+
+func buildVersion(version, commit, date, builtBy string) string {
+	var result = version
+	if commit != "" {
+		result = fmt.Sprintf("%s\ncommit: %s", result, commit)
+	}
+	if date != "" {
+		result = fmt.Sprintf("%s\nbuilt at: %s", result, date)
+	}
+	if builtBy != "" {
+		result = fmt.Sprintf("%s\nbuilt by: %s", result, builtBy)
+	}
+	return result
 }
