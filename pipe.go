4a4220bba62000acdd57c36f604d1e2ce19f6b94
linyows <linyows@gmail.com>
2021-01-24 03:51:16 +0900

    show pipeline

--- pipe.go
+++ pipe.go
@@ -1,10 +1,14 @@
 package warp
 
 import (
+	"bufio"
 	"bytes"
+	"fmt"
 	"io"
 	"log"
 	"net"
+	"net/textproto"
+	"strings"
 	"sync"
 )
 
@@ -18,25 +22,54 @@ type Pipe struct {
 func (p *Pipe) Do() {
 	p.Req = new(bytes.Buffer)
 	p.Res = new(bytes.Buffer)
-
-	close := func() {
-		defer p.Dst.Close()
-		defer p.Src.Close()
-		defer log.Print("connection closed")
-	}
 	var once sync.Once
 
 	// src ===> dst
 	go func() {
 		w := io.MultiWriter(p.Dst, p.Req)
 		io.Copy(w, p.Src)
-		once.Do(close)
+		once.Do(p.close())
 	}()
 
 	// src <=== dst
 	go func() {
 		w := io.MultiWriter(p.Src, p.Res)
 		io.Copy(w, p.Dst)
-		once.Do(close)
+		once.Do(p.close())
 	}()
+}
+
+func (p *Pipe) data(b *bytes.Buffer) ([]string, error) {
+	var data []string
+	r := textproto.NewReader(bufio.NewReader(b))
+	for {
+		line, err := r.ReadLine()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return data, err
+		}
+		data = append(data, line)
+	}
+	return data, nil
+}
+
+func (p *Pipe) close() func() {
+	return func() {
+		defer p.Dst.Close()
+		defer p.Src.Close()
+		defer log.Print("connection closed")
+
+		req, err := p.data(p.Req)
+		if err != nil {
+			fmt.Printf("%#v\n", err)
+		}
+		fmt.Printf("%s\n", strings.Join(req, "\n"))
+		res, err := p.data(p.Res)
+		if err != nil {
+			fmt.Printf("%#v\n", err)
+		}
+		fmt.Printf("%s\n", strings.Join(res, "\n"))
+	}
 }
