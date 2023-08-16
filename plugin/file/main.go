7a54dcf4a1159970b9d6c495e8014879a19fe7ec
linyows <linyows@gmail.com>
2023-08-16 12:12:24 +0900

    add error as return variable and do not panic

--- plugin/file/main.go
+++ plugin/file/main.go
@@ -10,42 +10,55 @@ import (
 )
 
 const (
+	prefix   string = "file-plugin"
 	commJson string = "{\"type\":\"comm\",\"occurred_at\":\"%s\",\"connection_id\":\"%s\",\"direction\":\"%s\",\"data\":\"%s\"}\n"
-	connJson string = "{\"type\":\"conn\"\"occurred_at\":\"%s\",\"connection_id\":\"%s\",\"from\":\"%s\",\"to\":\"%s\"\n}"
+	connJson string = "{\"type\":\"conn\"\"occurred_at\":\"%s\",\"connection_id\":\"%s\",\"from\":\"%s\",\"to\":\"%s\",\"elapsed\":\"%s\"\n}"
 )
 
 type File struct {
 	file *os.File
 }
 
-func (f *File) writer() io.Writer {
+func (f *File) writer() (io.Writer, error) {
 	if f.file != nil {
-		return f.file
+		return f.file, nil
 	}
 
 	path := os.Getenv("FILE_PATH")
 	if len(path) == 0 {
-		panic("missing path for file")
+		return nil, fmt.Error("missing path for file, please set `FILE_PATH`")
 	}
 
 	var err error
 	f.file, err = os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		panic(err)
+		return nil, fmt.Errorf("os.OpenFile error: %#v\n", err)
 	}
 
-	return f.file
+	return f.file, nil
 }
 
 func (f *File) AfterComm(d *warp.AfterCommData) {
-	if _, err := fmt.Fprintf(f.writer(), commJson, d.OccurredAt.Format(time.RFC3339), d.ConnID, d.Direction, d.Data); err != nil {
-		fmt.Printf("file append error: %#v\n", err)
+	writer, err := f.writer()
+	if err != nil {
+		fmt.Printf("[%s] %#v\n", prefix, err)
+		return
+	}
+
+	if _, err := fmt.Fprintf(writer, commJson, d.OccurredAt.Format(time.RFC3339), d.ConnID, d.Direction, d.Data); err != nil {
+		fmt.Printf("[%s] file append error: %#v\n", prefix, err)
 	}
 }
 
 func (f *File) AfterConn(d *warp.AfterConnData) {
-	if _, err := fmt.Fprintf(f.writer(), connJson, d.OccurredAt.Format(time.RFC3339), d.ConnID, d.MailFrom, d.MailTo); err != nil {
-		fmt.Printf("file append error: %#v\n", err)
+	writer, err := f.writer()
+	if err != nil {
+		fmt.Printf("[%s] %#v\n", prefix, err)
+		return
+	}
+
+	if _, err := fmt.Fprintf(writer, connJson, d.OccurredAt.Format(time.RFC3339), d.ConnID, d.MailFrom, d.MailTo, d.ElapsedSeconds); err != nil {
+		fmt.Printf("[%s] file append error: %#v\n", prefix, err)
 	}
 }
 
