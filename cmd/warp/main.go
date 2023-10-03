da6ea0e22c84e95cf9d7c6dac791858a10fe2388
linyows <linyows@gmail.com>
2023-10-04 00:13:21 +0900

    add parse command option

--- cmd/warp/main.go
+++ cmd/warp/main.go
@@ -4,6 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 
 	"github.com/linyows/warp"
 )
@@ -15,7 +18,9 @@ var (
 	builtBy = ""
 	ip      = flag.String("ip", "127.0.0.1", "listen ip")
 	port    = flag.Int("port", 0, "listen port")
+	opr     = flag.String("opr", "", "outbound port range: 12000-12500")
 	verFlag = flag.Bool("version", false, "show build version")
+	oprRe   = regexp.MustCompile(`^([1-9][0-9]{0,5})-([1-9][0-9]{0,5})$`)
 )
 
 func init() {
@@ -27,7 +32,25 @@ func main() {
 		fmt.Fprintf(os.Stderr, buildVersion(version, commit, date, builtBy)+"\n")
 		return
 	}
+
 	w := &warp.Server{Addr: *ip, Port: *port}
+
+	trimedOpr := strings.TrimSpace(*opr)
+	if trimedOpr != "" {
+		matched := oprRe.FindStringSubmatch(trimedOpr)
+		if len(matched) == 3 {
+			s, _ := strconv.Atoi(matched[1])
+			e, _ := strconv.Atoi(matched[2])
+			w.OutboundPorts = &warp.PortRange{
+				Start: s,
+				End:   e,
+			}
+		} else {
+			fmt.Fprintf(os.Stderr, "outbound-port-range option format is <number>-<number>\n")
+			return
+		}
+	}
+
 	err := w.Start()
 	if err != nil {
 		panic(err)
